b3bf34af64ac4e0c33897693465726f12a41ca77
Newtonkamau14 <newtonkamau14@gmail.com>
2024-07-08 16:55:40 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="bg-slate-200 min-h-screen">
+      <Navbar />
+      <p className="h-screen flex justify-center items-center text-6xl text-black text-center ">React App with voiceflow chatbot</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      <a href="/" className="p-4">React Voiceflow</a>
+    </div>
+  );
+}
+
+export default Navbar;
